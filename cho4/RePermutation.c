e8815c1a4739bed03c039f4299940631a8ce10e9
pieunseo <jpmdyh21@gmail.com>
2024-12-02 14:46:12 +0900

    순열구하기 및 12-02전까지 작성한 코드들

--- cho4/RePermutation.c
+++ cho4/RePermutation.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
-#define R 3
-int b[R], cnt = 0;
 
-void printRePer(void) {
-    for (int i = 0; i < R; i++) printf("%5d", b[i]);
-    printf("\n"); cnt++;
+#define R 3 // 고정된 R의 크기
+int b[R] = { 0 }; // 선택된 요소를 저장할 배열
+int cnt = 0;    // 순열 개수를 세는 변수
+
+void PrintPer() {
+    for (int i = 0; i < R; i++) {
+        printf("%d ", b[i]);
+    }
+    printf("\n");
+    cnt++;
 }
 
-RePermutation(int a[], int n, int r) {
-    int i;
-    if (r == R) printRePer();
-    else for (i = 0; i < n; i++) {
-        b[r] = a[i];
-        RePermutation(a, n, r + 1);
+void exchange(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void Permutation(int a[], int n, int r) {
+    if (r >= R) {
+        PrintPer();
+    }
+    else {
+        for (int i = r; i < n; i++) {
+            exchange(a + r, a + i); // 요소 교환
+            b[r] = a[r];           // 선택된 요소 저장
+            Permutation(a, n, r + 1); // 재귀 호출
+            exchange(a + r, a + i); // 원래 상태로 복원
+        }
     }
 }
-int main(void) {
-    int a[] = { 1, 2, 3 ,4, 5 };
-    int n = sizeof(a) / sizeof(int);
 
-    RePermutation(a, n, 0);
-    printf("%d", cnt);
+int main(void) {
+    int a[] = { 1, 2, 3, 4, 5 }; // 입력 배열
+    int n = sizeof(a) / sizeof(int); // 배열 크기 계산
+    Permutation(a, n, 0); // 순열 생성 시작
+    printf("순열 개수 : %d\n", cnt);
     return 0;
 }
